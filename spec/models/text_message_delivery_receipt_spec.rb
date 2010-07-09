ddfc397ce72010f9844b5cb4389d284738b45b7b
David Wilkie <dwilkie@gmail.com>
2010-07-09 16:41:58 +0700

    updated spec to reflect new validations

--- spec/models/text_message_delivery_receipt_spec.rb
+++ spec/models/text_message_delivery_receipt_spec.rb
@@ -2,16 +2,32 @@ require 'spec_helper'
 
 describe TextMessageDeliveryReceipt do
   describe "validations" do
-    it "should not be valid without an associated outgoing text message" do
-      text_message_delivery_receipt = Factory.build(:text_message_delivery_receipt)
-      text_message_delivery_receipt.outgoing_text_message = nil
-      text_message_delivery_receipt.should_not be_valid
-    end
     it "should not be valid without params" do
       text_message_delivery_receipt = Factory.build(:text_message_delivery_receipt)
       text_message_delivery_receipt.params = nil
       text_message_delivery_receipt.should_not be_valid
+      text_message_delivery_receipt.errors_on(:params).should_not be_empty
     end
+    it "should not be valid without an associated outgoing text message" do
+      text_message_delivery_receipt = Factory.build(:text_message_delivery_receipt)
+      text_message_delivery_receipt.params["msgid"] = ""
+      text_message_delivery_receipt.should_not be_valid
+      text_message_delivery_receipt.errors_on(
+        :outgoing_text_message
+      ).should_not be_empty
+    end
+  end
+  it "should not allow two identical text message delivery receipts to be created" do
+    text_message_delivery_receipt = Factory.create(
+      :text_message_delivery_receipt
+    )
+    duplicate_text_message_delivery_receipt = Factory.build(
+      :text_message_delivery_receipt
+    )
+    duplicate_text_message_delivery_receipt.params = text_message_delivery_receipt.params
+    lambda {
+      duplicate_text_message_delivery_receipt.save!
+    }.should raise_error
   end
 end
 
