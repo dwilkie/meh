91c3fb07a7a552b74e328e8e7bf76ac012669844
David Wilkie <dwilkie@gmail.com>
2010-09-14 18:33:39 +0700

    Added live test to simulate an incoming text message

---
+++ live_testing/incoming_text_message.rb
@@ -0,0 +1,15 @@
+# Simulates an incoming text message
+puts "Enter your text message and press 'ENTER'"
+message = gets.chomp
+default_seller_number = Test::PARAMS["seller_mobile_number".to_sym]
+default_supplier_number = Test::PARAMS["seller_mobile_number".to_sym]
+puts "Enter the number to send from: Type: '1' for: '#{default_seller_number}' or '2' for: #{default_supplier_number}"
+number = gets.chomp
+case number
+  when '1'
+    number = default_seller_number
+  when '2'
+    number = default_supplier_number
+end
+#exec("curl", "-D "
+
