211a97a5e982898af25f92956c1e2eecd8745d7b
David Wilkie <dwilkie@gmail.com>
2010-09-14 18:11:26 +0700

    Added script to add mobile numbers for live testing

---
+++ live_testing/create_mobile_numbers.rb
@@ -0,0 +1,19 @@
+# Usage
+# rails runner create_mobile_numbers.rb
+require File.expand_path(File.dirname(__FILE__) + '/live_testing')
+
+def create_mobile_number(role)
+  default_number = Test::PARAMS["#{role.to_s}_mobile_number".to_sym]
+  puts "Enter the #{role.to_s.upcase}'S number or press 'ENTER'. Type: 'd' for default: '#{default_number}'"
+  response = gets.chomp
+  number = response == "d" ? default_number : response
+  number = Test.create_mobile_number(role, String.new(number)) unless number.blank?
+end
+
+puts "This script will create mobile numbers which will trigger text messages to be sent. THIS WILL COST MONEY. Type 'yes' to continue..."
+response = gets
+if response.chomp.downcase == "yes"
+  create_mobile_number(:seller)
+  create_mobile_number(:supplier)
+end
+
